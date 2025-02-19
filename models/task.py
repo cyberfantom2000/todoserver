42fcbe039a06e64cf8a51a6cf4f4328b4569dcda
Daniil <tak.sebek@yandex.ru>
2025-02-20 00:24:48 +0300

    change task model

--- models/task.py
+++ models/task.py
@@ -8,8 +8,11 @@ class TaskBase(SQLModel):
     is_done: bool = Field(default=False)
     content: str
     deadline: datetime.datetime | None = Field(default=None)
+    remind: datetime.datetime | None = Field(default=None)
     is_trash: bool = Field(index=True, default=False)
     parent_id: uuid.UUID | None = Field(default=None, foreign_key='task.id', ondelete='CASCADE')
+    priority: int = Field(default=1)  # 1 - 4
+    topic: str | None = Field(default=None)
 
 
 class Task(TaskBase, table=True):
@@ -33,3 +36,7 @@ class TaskUpdate(TaskBase):
     is_trash: bool | None = None
     content: str | None = None
     deadline: datetime.datetime | None = None
+    remind: datetime.datetime | None = None
+    parent_id: uuid.UUID | None = None
+    topic: str | None = None
+    priority: int | None = None
